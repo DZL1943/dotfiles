49d18f80b07b963a0ccec334a8b5bab05b02752d
mac <mac@macdeMacBook-Air.local>
2024-06-26 16:49:51 +0800

    vault backup: 2024-06-26 16:49:51
    Affected files:
    .obsidian/community-plugins.json
    .obsidian/plugins/obsidian42-brat/data.json
    .obsidian/workspace.json
    mac-mouse-fix/config.plist
    main.py
    toolset.yaml

--- main.py
+++ main.py
@@ -15,14 +15,16 @@ def backup(config, *apps):
             for f1, f2 in list(data.get('files').items()):
                 f1 = os.path.expanduser(f1)
                 f2 = os.path.expanduser(f2)
+                print(f'{f1} {f2}')
                 if os.path.exists(f1):
                     f2 = os.path.join(app, f2)
-                    if not os.path.isdir(f2):
+                    if os.path.isfile(f2):
                         f2 = f2 + '~'
                     if not os.path.exists(os.path.dirname(f2)):
                         # os.system(f'mkdir -p {os.path.dirname(f2)}')
                         pass
-                    os.system(f'echo {f1} {f2}')
+                    os.system(f'echo "{f1}" "{f2}"')
+                    os.system(f'gcp -v "{f1}" "{f2}"')
 
 def restore(config, *apps):
     for app in apps:
@@ -31,6 +33,7 @@ def restore(config, *apps):
             for f1, f2 in list(data.get('files').items()):
                 f1 = os.path.expanduser(f1)
                 f2 = os.path.expanduser(f2)
+                print(f'{f1} {f2}')
                 if f1.endswith('/.'):
                     f1 = os.path.dirname(f1)
                     f2 = os.path.join(app, f2, '.')
@@ -39,7 +42,8 @@ def restore(config, *apps):
                 if not os.path.exists(os.path.dirname(f1)):
                     # os.system(f'mkdir -p {os.path.dirname(f1)}')
                     pass
-                os.system(f'echo {f2} {f1}')
+                os.system(f'echo "{f2}" "{f1}"')
+                os.system(f'gcp -v "{f2}" "{f1}"')
 
 
 def main():
