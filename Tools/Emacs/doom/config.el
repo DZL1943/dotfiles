3cc489307de16b0c6bbf3efa1788a01051270000
mac <mac@macdeMacBook-Air.local>
2024-07-05 01:00:22 +0800

    .

--- Tools/Emacs/doom/config.el
+++ Tools/Emacs/doom/config.el
@@ -1,3 +1,4 @@
+(setq confirm-kill-emacs nil)
 ;; M-x nerd-icons-install-fonts
 (setq doom-font (font-spec :family "Hack Nerd Font" :size 20 :weight 'semi-light)
 )
@@ -11,4 +12,8 @@
     (width . (text-pixels . ,(round (*    (x-display-pixel-width) 0.72))))    (height . (text-pixels . ,(round (*    (x-display-pixel-height) 0.84))))
   ))
   (set-frame-parameter nil 'alpha 85)
+  ;; :ui tabs 中的 centaur-tabs 有问题不显示; tab-bar-mode 用不习惯
+  ;; (global-tab-line-mode 1)
+  ;; 有个问题是一些虚拟的 buff 不支持点击
+  ;; (setq doom-unreal-buffer-functions '(minibufferp))
 )
