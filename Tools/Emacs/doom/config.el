bd59ca5628d1b2c95fbecf325109603092603e01
mac <mac@macdeMacBook-Air.local>
2024-07-04 18:14:45 +0800

    .

--- Tools/Emacs/doom/config.el
+++ Tools/Emacs/doom/config.el
@@ -2,9 +2,11 @@
 (setq doom-font (font-spec :family "Hack Nerd Font" :size 20 :weight 'semi-light)
 )
 (setq doom-theme 'doom-one)
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
 (setq org-directory "~/org/")
+(setq default-directory "~/org/")
 (setq initial-frame-alist `(
       (top . 20) (left . 150)
       (width . (text-pixels . ,(round (* (x-display-pixel-width) 0.72)))) (height . (text-pixels . ,(round (* (x-display-pixel-height) 0.84))))
 ))
+(set-frame-parameter nil 'alpha 85)
