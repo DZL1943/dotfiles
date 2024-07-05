9335f5324627ac8b554536dd5ee79296a2217d2b
mac <mac@macdeMacBook-Air.local>
2024-07-05 18:13:03 +0800

    .

--- Tools/Emacs/doom/config.el
+++ Tools/Emacs/doom/config.el
@@ -1,4 +1,5 @@
 (setq confirm-kill-emacs nil)
+(add-hook 'after-init-hook 'doom/switch-to-scratch-buffer)
 ;; M-x nerd-icons-install-fonts
 (setq doom-font (font-spec :family "Hack Nerd Font" :size 20 :weight 'semi-light)
 )
@@ -12,8 +13,8 @@
     (width . (text-pixels . ,(round (*    (x-display-pixel-width) 0.72))))    (height . (text-pixels . ,(round (*    (x-display-pixel-height) 0.84))))
   ))
   (set-frame-parameter nil 'alpha 85)
-  ;; :ui tabs 中的 centaur-tabs 有问题不显示; tab-bar-mode 用不习惯
   ;; (global-tab-line-mode 1)
-  ;; 有个问题是一些虚拟的 buff 不支持点击
   ;; (setq doom-unreal-buffer-functions '(minibufferp))
+  ;; :ui tabs not display
+  ;; (after! centaur-tabs (centaur-tabs-group-by-projectile-project))
 )
