099066947842faf72e47909b1dcc6bc61c1c7a2b
mac <mac@macdeMacBook-Air.local>
2024-07-07 11:02:45 +0800

    .

--- Tools/Neovim/lua/plugins/custom.lua
+++ Tools/Neovim/lua/plugins/custom.lua
@@ -7,4 +7,25 @@ return {
             },
         },
     },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = {
+                "lua_ls",
+                "tsserver",
+                "pyright",
+                "rust_analyzer",
+                -- "jdtls",
+                -- "clangd",
+                -- "bashls",
+                -- "html",
+                -- "cssls",
+                -- "jsonls",
+                -- "yamlls",
+                -- "marksman",
+                -- "sqls",
+            },
+            -- handlers = nil,
+        }
+    },
 }
