fdcdf17b644c4348db7fc8557640170d997e7e2d
mac <mac@macdeMacBook-Air.local>
2024-07-15 17:17:34 +0800

    .

--- Tools/Neovim/lua/plugins/custom.lua
+++ Tools/Neovim/lua/plugins/custom.lua
@@ -1,31 +1,32 @@
 return {
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        opts = {
-            window = {
-                width = 20,
-            },
-        },
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        opts = {
-            ensure_installed = {
-                "lua_ls",
-                "tsserver",
-                "pyright",
-                "rust_analyzer",
-                -- "jdtls",
-                -- "clangd",
-                -- "bashls",
-                -- "html",
-                -- "cssls",
-                -- "jsonls",
-                -- "yamlls",
-                -- "marksman",
-                -- "sqls",
-            },
-            -- handlers = nil,
-        }
-    },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			window = {
+				width = 20,
+			},
+		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"tsserver",
+				"pyright",
+				-- "rust_analyzer",
+				-- "jdtls",
+				-- "clangd",
+				-- "bashls",
+				-- "html",
+				-- "cssls",
+				-- "jsonls",
+				-- "yamlls",
+				-- "marksman",
+				-- "sqls",
+			},
+			-- handlers = nil,
+		},
+	},
 }
+
